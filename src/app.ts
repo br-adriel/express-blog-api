c85b7341c18eb377edf0e485215b37c2569e7394
Adriel <adriel.fsantos@outlook.com>
2023-03-05 14:12:03 -0300

    Implementa conexão com o banco de dados

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
+import { connectToDatabase } from './database';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
+// Inicia conexão com o banco de dados
+connectToDatabase();
+
+// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`SERVER RUNNING => http://localhost:${PORT}/`);
 });
