65a15bdb33194b2d71a879209ce0f91a57adc1db
Adriel <adriel.fsantos@outlook.com>
2023-03-07 18:44:50 -0300

    Adiciona modelo Post

---
+++ src/models/Post.ts
@@ -0,0 +1,23 @@
+import { model, Schema, Types } from 'mongoose';
+import { IUser } from './User';
+
+export interface IPost {
+  title: string;
+  content: string;
+  author: IUser | Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const PostSchema = new Schema<IPost>(
+  {
+    author: { type: Schema.Types.ObjectId, required: true },
+    content: { type: String, required: true, minlength: 1 },
+    title: { type: String, required: true, minlength: 1 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model<IPost>('Post', PostSchema);
