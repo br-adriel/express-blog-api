09a91183d84a52f00f740e0b4abdd43915631d4e
Adriel <adriel.fsantos@outlook.com>
2023-03-07 21:32:00 -0300

    Insere rotas no app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
 import { connectToDatabase } from './database';
+import commentRoutes from './routes/commentRoutes';
+import postRoutes from './routes/postRoutes';
+import userRoutes from './routes/userRoutes';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+// Rotas
+app.use('/comments', commentRoutes);
+app.use('/posts', postRoutes);
+app.use('/users', userRoutes);
 
 // Inicia conexão com o banco de dados
 connectToDatabase();
