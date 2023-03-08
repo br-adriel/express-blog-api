94a5c705aacc90a7c92c8e4a95bdc1c9d3af7f09
Adriel <adriel.fsantos@outlook.com>
2023-03-07 21:26:27 -0300

    Adiciona rotas para usuários

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import UserController from '../controllers/userController';
+
+const userRoutes = Router();
+const userController = new UserController();
+
+userRoutes.get('/', userController.getUsers);
+userRoutes.post('/', userController.createUser);
+
+userRoutes.get('/:id', userController.getUser);
+userRoutes.patch('/:id', userController.updateUser);
+userRoutes.delete('/:id', userController.removeUser);
+
+userRoutes.patch('/:id/author', userController.toggleUserAuthorPrivileges);
+
+userRoutes.patch('/:id/admin', userController.toggleUserAdminrPrivileges);
+
+userRoutes.post('/authenticate', userController.login);
