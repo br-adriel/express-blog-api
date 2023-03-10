c23123029c32533397b0201a0204ce91ed747f92
Adriel <adriel.fsantos@outlook.com>
2023-03-10 15:57:17 -0300

    Implementa createPost

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 import { StatusCodes } from 'http-status-codes';
+import { errorFormatter } from '../lib/express-validator';
 import Post from '../models/Post';
 
 export default class PostController {
@@ -36,20 +38,47 @@ export default class PostController {
    *
    * @param {Request} req Espera os campos title e content no seu body
    */
-  async createPost(
-    req: Request<
-      {},
-      {},
-      {
-        title: string;
-        content: string;
+  createPost = [
+    body('title')
+      .isLength({ min: 1 })
+      .withMessage('Insira um título em sua postagem'),
+
+    body('content')
+      .isLength({ min: 1 })
+      .withMessage('Adicione conteúdo a sua postagem'),
+
+    async (
+      req: Request<
+        {},
+        {},
+        {
+          title: string;
+          content: string;
+        }
+      >,
+      res: Response,
+      next: NextFunction
+    ) => {
+      const errors = validationResult(req).formatWith(errorFormatter);
+      if (!errors.isEmpty()) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ errors: errors.array });
       }
-    >,
-    res: Response,
-    next: NextFunction
-  ) {
-    return res.sendStatus(201);
-  }
+
+      try {
+        const post = new Post({
+          title: req.body.title,
+          content: req.body.content,
+          author: req.user!.id,
+        });
+        await post.save();
+        return res.sendStatus(StatusCodes.CREATED);
+      } catch (error) {
+        return next(error);
+      }
+    },
+  ];
 
   /**
    * Atualiza o post com a id correspondente
