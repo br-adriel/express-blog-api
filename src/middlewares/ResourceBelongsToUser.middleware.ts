74307fae602b9fa005341cced06f1088e1379f7f
Adriel <adriel.fsantos@outlook.com>
2023-03-10 15:45:26 -0300

    Implementa middleware PostBelongsToUser

--- src/middlewares/ResourceBelongsToUser.middleware.ts
+++ src/middlewares/ResourceBelongsToUser.middleware.ts
@@ -1,14 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import Post from '../models/Post';
 import User from '../models/User';
 
 /**
  * Garante que o usuário que está solicitando o acesso a um objeto 'User' tem
  * permissão para acessá-lo (se é dono dele ou admin)
- *
- * @param req
- * @param res
- * @param next
  */
 export async function UserBelongsToUser(
   req: Request<{ id: string }>,
@@ -23,6 +20,33 @@ export async function UserBelongsToUser(
       if (!requestedUser) return res.sendStatus(StatusCodes.NOT_FOUND);
 
       if (req.user!.id !== requestedUser.id)
+        return res.sendStatus(StatusCodes.FORBIDDEN);
+      return next();
+    } catch (error) {
+      next(error);
+    }
+  }
+  return res.sendStatus(StatusCodes.UNAUTHORIZED);
+}
+
+/**
+ * Garante que o usuário que está solicitando o acesso a um objeto 'Post' tem
+ * permissão para acessá-lo (se é seu autor ou admin)
+ */
+export async function PostBelongsToUser(
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.user) {
+    if (req.user.isAdmin) return next();
+
+    try {
+      const requestedPost = await Post.findById(req.params.id);
+      if (!requestedPost) return res.sendStatus(StatusCodes.NOT_FOUND);
+
+      console.log(requestedPost.author.toString());
+      if (req.user!.id !== requestedPost.author.toString())
         return res.sendStatus(StatusCodes.FORBIDDEN);
       return next();
     } catch (error) {
