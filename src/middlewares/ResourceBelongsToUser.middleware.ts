968d515596a0f5e2a070c46ed43cb597baad93e6
Adriel <adriel.fsantos@outlook.com>
2023-03-10 21:06:37 -0300

    Implementa middleware CommentBelongsToUser

--- src/middlewares/ResourceBelongsToUser.middleware.ts
+++ src/middlewares/ResourceBelongsToUser.middleware.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import Comment from '../models/Comment';
 import Post from '../models/Post';
 import User from '../models/User';
 
@@ -46,6 +47,32 @@ export async function PostBelongsToUser(
       if (!requestedPost) return res.sendStatus(StatusCodes.NOT_FOUND);
 
       if (req.user!.id !== requestedPost.author.toString())
+        return res.sendStatus(StatusCodes.FORBIDDEN);
+      return next();
+    } catch (error) {
+      next(error);
+    }
+  }
+  return res.sendStatus(StatusCodes.UNAUTHORIZED);
+}
+
+/**
+ * Garante que o usuário que está solicitando o acesso a um objeto 'Comment' tem
+ * permissão para acessá-lo (se é seu autor ou admin)
+ */
+export async function CommentBelongsToUser(
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.user) {
+    if (req.user.isAdmin) return next();
+
+    try {
+      const requestedComment = await Comment.findById(req.params.id);
+      if (!requestedComment) return res.sendStatus(StatusCodes.NOT_FOUND);
+
+      if (req.user!.id !== requestedComment.author.toString())
         return res.sendStatus(StatusCodes.FORBIDDEN);
       return next();
     } catch (error) {
