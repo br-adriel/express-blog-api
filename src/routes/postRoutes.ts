718353bbffdd87681184b3628879bee067fa392f
Adriel <adriel.fsantos@outlook.com>
2023-03-11 17:26:44 -0300

    Insere UserMustBeAuthor em rota de criação de post

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -3,13 +3,19 @@ import CommentController from '../controllers/commentController';
 import PostController from '../controllers/postController';
 import { PostBelongsToUser } from '../middlewares/ResourceBelongsToUser.middleware';
 import UserMustBeAuthenticated from '../middlewares/UserMustBeAuthenticated.middleware';
+import { UserMustBeAuthor } from '../middlewares/UserPrivileges.middleware';
 
 const postRoutes = Router();
 const postController = new PostController();
 const commentController = new CommentController();
 
 postRoutes.get('/', postController.getPosts);
-postRoutes.post('/', UserMustBeAuthenticated, postController.createPost);
+postRoutes.post(
+  '/',
+  UserMustBeAuthenticated,
+  UserMustBeAuthor,
+  postController.createPost
+);
 
 postRoutes.get('/:id', postController.getPost);
 postRoutes.patch(
